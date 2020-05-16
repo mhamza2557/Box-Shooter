ec07e3f030dd3eef61df09a30e010031cf4870d6
Hamza <mhamza2551@gmail.com>
2020-05-17 03:09:37 +0500

    Update MouseLooker.cs

--- Assets/Scripts/MouseLooker.cs
+++ Assets/Scripts/MouseLooker.cs
@@ -2,52 +2,46 @@
 using System.Collections;
 
 public class MouseLooker : MonoBehaviour {
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
 
-	// Use this for initialization
-	public float XSensitivity = 2f;
-	public float YSensitivity = 2f;
-	public bool clampVerticalRotation = true;
-	public float MinimumX = -90F;
-	public float MaximumX = 90F;
-	public bool smooth;
-	public float smoothTime = 5f;
-	
-	// internal private variables
-	private Quaternion m_CharacterTargetRot;
-	private Quaternion m_CameraTargetRot;
-	private Transform character;
-	private Transform cameraTransform;
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
 
-	void Start() {
-		// start the game with the cursor locked
-		LockCursor (true);
+    void Start()
+    {
+        LockCursor(true);
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
 
-		// get a reference to the character's transform (which this script should be attached to)
-		character = gameObject.transform;
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
 
-		// get a reference to the main camera's transform
-		cameraTransform = Camera.main.transform;
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
-		// get the location rotation of the character and the camera
-		m_CharacterTargetRot = character.localRotation;
-		m_CameraTargetRot = cameraTransform.localRotation;
-	}
-	
-	void Update() {
-		// rotate stuff based on the mouse
-		LookRotation ();
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
 
-		// if ESCAPE key is pressed, then unlock the cursor
-		if(Input.GetButtonDown("Cancel")){
-			LockCursor (false);
-		}
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
 
-		// if the player fires, then relock the cursor
-		if(Input.GetButtonDown("Fire1")){
-			LockCursor (true);
-		}
-	}
-	
+        // if ESCAPE key is pressed, then unlock the cursor
+        if (Input.GetButtonDown("Cancel"))
+        {
+            LockCursor(false);
+        }
+
+        // if the player fires, then relock the cursor
+        if (Input.GetButtonDown("Fire1"))
+        {
+            LockCursor(true);
+        }
+    }
+    
 	private void LockCursor(bool isLocked)
 	{
 		if (isLocked) 
@@ -66,49 +60,4 @@ public class MouseLooker : MonoBehaviour {
 		}
 	}
 
-	public void LookRotation()
-	{
-		//get the y and x rotation based on the Input manager
-		float yRot = Input.GetAxis("Mouse X") * XSensitivity;
-		float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
-
-		// calculate the rotation
-		m_CharacterTargetRot *= Quaternion.Euler (0f, yRot, 0f);
-		m_CameraTargetRot *= Quaternion.Euler (-xRot, 0f, 0f);
-
-		// clamp the vertical rotation if specified
-		if(clampVerticalRotation)
-			m_CameraTargetRot = ClampRotationAroundXAxis (m_CameraTargetRot);
-
-		// update the character and camera based on calculations
-		if(smooth) // if smooth, then slerp over time
-		{
-			character.localRotation = Quaternion.Slerp (character.localRotation, m_CharacterTargetRot,
-			                                            smoothTime * Time.deltaTime);
-			cameraTransform.localRotation = Quaternion.Slerp (cameraTransform.localRotation, m_CameraTargetRot,
-			                                         smoothTime * Time.deltaTime);
-		}
-		else // not smooth, so just jump
-		{
-			character.localRotation = m_CharacterTargetRot;
-			cameraTransform.localRotation = m_CameraTargetRot;
-		}
-	}
-	
-	// Some math ... eeck!
-	Quaternion ClampRotationAroundXAxis(Quaternion q)
-	{
-		q.x /= q.w;
-		q.y /= q.w;
-		q.z /= q.w;
-		q.w = 1.0f;
-		
-		float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
-		
-		angleX = Mathf.Clamp (angleX, MinimumX, MaximumX);
-		
-		q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
-		
-		return q;
-	}
 }
