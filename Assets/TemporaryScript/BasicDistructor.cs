5e48eb46482bc6f11973c4f6944bde02fc019e09
Hamza <mhamza2551@gmail.com>
2020-05-21 04:33:05 +0500

    Create BasicDistructor.cs

---
+++ Assets/TemporaryScript/BasicDistructor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicDistructor : MonoBehaviour
+{
+    public float timeOut = 1.0f;
+    public bool detachChildren = true;
+
+    private void Awake() {
+        Invoke("DestroyNow", timeOut);
+    }
+
+    void DestroyNow() {
+        if (detachChildren)
+        {
+            transform.DetachChildren();
+        }
+
+        Destroy(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
