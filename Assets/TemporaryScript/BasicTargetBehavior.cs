2fd9f4b1fd497d69dba9b9906e28489fe62d99e4
Hamza <mhamza2551@gmail.com>
2020-06-07 12:34:17 +0500

    Create BasicTargetBehavior.cs

---
+++ Assets/TemporaryScript/BasicTargetBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetBehavior : MonoBehaviour
+{
+    public int scoreAmount = 0;
+    public float timeAmount = 0.0f;
+
+    public GameObject explosionPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (GameManager.gm) {
+            if (GameManager.gm.gameIsOver) {
+                return;
+            }
+        }
+
+        if (collision.gameObject.tag == "Projectile") {
+            if (explosionPrefab) {
+                Instantiate(explosionPrefab, transform.position, transform.rotation);
+            }
+
+            if (GameManager.gm) {
+                GameManager.gm.targetHit(scoreAmount, timeAmount);
+            }
+
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
