4ef6eaa5f5b454e86a08d875ff1674e15dc27497
Hamza <mhamza2551@gmail.com>
2020-06-07 15:09:37 +0500

    Create BasicTargetExit.cs

---
+++ Assets/TemporaryScript/BasicTargetExit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetExit : MonoBehaviour
+{
+    public float exitAfterSenonds = 10f;
+    public float exitAnimationSeconds = 1f;
+
+    private bool startDestroy = false;
+    private float targetTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetTime = Time.time + exitAfterSenonds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= targetTime) {
+            if (this.GetComponent<Animator>() == null) {
+                Destroy(gameObject);
+            } else if (!startDestroy) {
+                startDestroy = true;
+                this.GetComponent<Animator>().SetTrigger("Exit");
+                Invoke("KillTarget", exitAnimationSeconds);
+            }
+        }
+    }
+
+    public void KillTarget() {
+        Destroy(gameObject);
+    }
+}
